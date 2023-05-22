54c7e847bdb75e5c3d2131f79a0ac0765ef550b5
Oliver Wiebeck <wiebeck@riege.com>
2023-05-22 18:45:27 +0200

    Proper check annotations

--- modules/foo/main.tf
+++ modules/foo/main.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
-resource "random_id" "server" {
-  byte_length = 8
-}
-
 output "server_id" {
   value = random_id.server.hex
   description = "The server id"
+}
+
+resource "random_id" "server" {
+  byte_length = 8
 }
