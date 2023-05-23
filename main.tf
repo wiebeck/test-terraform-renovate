cb1b43c5d2d6a62c92a8af1f26acb68f772af645
Oliver Wiebeck <wiebeck@riege.com>
2023-05-23 12:59:21 +0200

    Move random provider to child module

--- main.tf
+++ main.tf
@@ -4,14 +4,7 @@ terraform {
       source  = "hashicorp/null"
       version = ">= 3.2.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.0"
-    }
   }
-}
-
-provider "random" {
 }
 
 resource "null_resource" "example" {
--- modules/foo/main.tf
+++ modules/foo/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     random = {
       source  = "hashicorp/random"
+      version = ">= 3.5.0"
     }
   }
 }
